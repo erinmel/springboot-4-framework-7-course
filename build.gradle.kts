0294f893ee282f6f439cf2a060c7fc20d32043a6
Erin Melgoza De La Torre <amelgoza@insulet.com>
2026-08-11 15:39:20 -0600

    feat: spotless pluging for code style

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.diffplug.spotless") version "8.9.0"
 }
 
 group = "com.emelgoza"
@@ -12,6 +13,15 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
     }
+}
+
+spotless.java {
+    targetExclude("build/**")
+    target("src/**/*.java")
+    googleJavaFormat("1.36.1")
+    removeUnusedImports()
+    trimTrailingWhitespace()
+
 }
 
 repositories {
--- src/main/java/com/emelgoza/sfudemy/SfudemyApplication.java
+++ src/main/java/com/emelgoza/sfudemy/SfudemyApplication.java
@@ -9,5 +9,4 @@ public class SfudemyApplication {
   public static void main(String[] args) {
     SpringApplication.run(SfudemyApplication.class, args);
   }
-
 }
